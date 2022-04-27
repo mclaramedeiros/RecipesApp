ad714e99da4fb1ec25610f3a9ee2120b623dde2d
Jhon Santana <jhonsstn@gmail.com>
2022-04-27 10:54:38 -0300

    fix(ProfileIcon.js): desabilita search input no profile - Adicionado função que desabilita o campo de busca quando se é direcionado para o perfil com a busca ativa em tela.

--- src/components/ProfileIcon.js
+++ src/components/ProfileIcon.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../context/AppContext';
 import profileIcon from '../images/profileIcon.svg';
 
 function ProfileIcon() {
+  const { setSearchBarStatus } = useContext(Context);
   return (
-    <Link to="/profile">
+    <Link to="/profile" onClick={ () => setSearchBarStatus(false) }>
       <img
         data-testid="profile-top-btn"
         src={ profileIcon }
