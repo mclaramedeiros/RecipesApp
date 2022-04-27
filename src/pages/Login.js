aca67d7f0e5154dc4fad1a05ca2e43ed64ae4d9a
Jhon Santana <jhonsstn@gmail.com>
2022-04-27 10:48:51 -0300

    refactor(Login.js): ajustado uso do history - Alterado history do hook useHistory para para uso de props history - Alterados nomes e valores de algumas variaveis.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 
-function Login() {
+function Login({ history }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [btnStatus, setBtnStatus] = useState(true);
-  const history = useHistory();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,9 +15,9 @@ function Login() {
   };
 
   useEffect(() => {
-    const validate = () => {
-      const MIN_LENGTH_VALUE = 6;
-      const minPassValid = password.length > MIN_LENGTH_VALUE;
+    const formValidation = () => {
+      const MIN_LENGTH_VALUE = 7;
+      const minPassValid = password.length >= MIN_LENGTH_VALUE;
       const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       const isEmailValid = regexEmail.test(email);
       const isValid = minPassValid && isEmailValid;
@@ -28,7 +27,7 @@ function Login() {
         setBtnStatus(true);
       }
     };
-    validate();
+    formValidation();
   }, [password, email]);
 
   return (
@@ -63,5 +62,11 @@ function Login() {
     </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
