74471aa3413a580fa613f7898eb39cb003ce6cbf
Maria Clara <claramedeiroswon@gmail.com>
2022-05-09 18:47:46 -0300

    tela de receitas feitas

---
+++ src/components/DoneRecipesCard.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+// import ShareButton from './ShareButton';
+import shareIcon from '../images/shareIcon.svg';
+
+function DoneRecipesCard({ value, index, share, shareButton }) {
+  const history = useHistory();
+  return (
+    <div key={ index }>
+      <button
+        type="button"
+        onClick={ () => history.push(`/${value.type}s/${value.id}`) }
+      >
+        <img
+          src={ value.image }
+          alt={ value.name }
+          data-testid={ `${index}-horizontal-image` }
+          width="100px"
+        />
+      </button>
+
+      <p data-testid={ `${index}-horizontal-top-text` }>
+        {`${value.nationality} - ${value.category}`}
+      </p>
+      <p data-testid="1-horizontal-top-text">{value.alcoholicOrNot}</p>
+      <button
+        type="button"
+        onClick={ () => history.push(`/${value.type}s/${value.id}`) }
+      >
+        <h1 data-testid={ `${index}-horizontal-name` }>{value.name}</h1>
+      </button>
+
+      <p data-testid={ `${index}-horizontal-done-date` }>{value.doneDate}</p>
+      <h3>tags:</h3>
+      {value.tags.map((tag, i) => (
+        <span data-testid={ `${index}-${tag}-horizontal-tag` } key={ i }>
+          {tag}
+        </span>
+      ))}
+      <button type="button" onClick={ () => shareButton(value.id, value.type) }>
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          alt={ value.name }
+        />
+        {share && 'Link copied!'}
+      </button>
+    </div>
+  );
+}
+
+DoneRecipesCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  share: PropTypes.bool.isRequired,
+  shareButton: PropTypes.func.isRequired,
+  value: PropTypes.shape({
+    alcoholicOrNot: PropTypes.string,
+    category: PropTypes.string,
+    doneDate: PropTypes.string,
+    id: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    nationality: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    type: PropTypes.string,
+  }).isRequired,
+};
+
+export default DoneRecipesCard;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import Header from '../layout/Header';
-import Main from '../layout/Main';
+import React, { useEffect, useState } from 'react';
+import DoneRecipesCard from '../components/DoneRecipesCard';
 
 function DoneRecipes() {
+  const [filter, setFilter] = useState('all');
+  const [share, setShare] = useState(false);
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  useEffect(() => {
+    const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    setDoneRecipes(recipes);
+  }, []);
+
+  const shareButton = async (id, type) => {
+    const url = `http://localhost:3000/${type}s/${id}`;
+    await navigator.clipboard.writeText(url);
+
+    setShare(true);
+  };
+  const recipesFilter = (recipes) => {
+    if (filter === 'all') {
+      return recipes;
+    }
+    return recipes.filter((recipe) => recipe.type === filter);
+  };
+  const renderCards = (allRecipes) => {
+    const filteredRecipes = recipesFilter(allRecipes);
+
+    return filteredRecipes.map((value, index) => (
+      <DoneRecipesCard
+        key={ index }
+        value={ value }
+        index={ index }
+        share={ share }
+        shareButton={ shareButton }
+      />
+    ));
+  };
+
   return (
-    <>
-      <Header title="Done Recipes" />
-      <Main />
-    </>
+    <div>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('all') }
+        name="all"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
+        name="food"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
+        name="drink"
+      >
+        Drink
+      </button>
+      {!doneRecipes ? 'carregando' : renderCards(doneRecipes)}
+    </div>
   );
 }
 
