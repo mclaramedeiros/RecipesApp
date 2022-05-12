2fd097f0cb7e7c6e88ba481f99cfc820f120dec9
Jhon Santana <jhonsstn@gmail.com>
2022-05-12 15:42:20 -0300

    feat:adicionada tela de explorar com devidos botoes

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../layout/Header';
 import Main from '../layout/Main';
 import Footer from '../layout/Footer';
 
 function Explore() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore" />
-      <Main />
+      <Main>
+        <button
+          data-testid="explore-foods"
+          type="button"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          data-testid="explore-drinks"
+          type="button"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </Main>
       <Footer />
     </>
   );
